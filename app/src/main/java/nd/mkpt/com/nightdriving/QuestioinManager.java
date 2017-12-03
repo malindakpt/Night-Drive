2d148fefe245a8f8532faf72793402e3734a350f
MalindaK <m.malinda@mubasher.net>
2017-12-03 18:36:07 +0530

    chk3

---
+++ app/src/main/java/nd/mkpt/com/nightdriving/QuestioinManager.java
@@ -0,0 +1,12 @@
+package nd.mkpt.com.nightdriving;
+
+/**
+ * Created by MalindaK on 12/3/2017.
+ */
+
+public class QuestioinManager {
+    public static int count = 0;
+    public static String getQuestion(int id) {
+        return "Question "+id;
+    }
+}
---
+++ app/src/main/java/nd/mkpt/com/nightdriving/SpeechRec.java
@@ -0,0 +1,8 @@
+package nd.mkpt.com.nightdriving;
+
+/**
+ * Created by MalindaK on 12/3/2017.
+ */
+
+public class SpeechRec  {
+}
